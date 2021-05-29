64d5c3b201784e93a9ab7950cf70b1bffeba201f
madhu2910 <78549424+madhu2910@users.noreply.github.com>
2021-05-29 15:23:37 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,59 +1,130 @@
-import  React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import React from 'react';
+import { View, Text, TextInput, SafeAreaView, TouchableOpacity, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
 
-import Login from './Component/Login';
-import Signup from './Component/Signup';
-import Verification from './Component/Verification';
 
+export default function  App () {
 
-const Stack = createStackNavigator();
+   
+    const styles = StyleSheet.create({
+        container: {
+            flex: 1,
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            padding: 35,
+            backgroundColor: "#ffcc00"
+          },
+        
+       
+       header: {
+           flexDirection: 'row',
+           
+           justifyContent: 'space-between',
+           alignItems: 'center',
+           color: '#000000',
+           padding: 20,
+       },
+       headerTitle: {
+           fontSize: 30,
+           fontWeight: 'bold',
+           color: '#000000',
 
-function MyStack() {
-  return (
-    <Stack.Navigator
-      initialRouteName="Signup"
-      screenOptions={{
-        headerTitleAlign: 'center',
-        headerStyle: {
-          backgroundColor: '#3740FE',
-        },
-        headerTintColor: '#fff',
-        headerTitleStyle: {
-          fontWeight: 'bold',
-        },
-      }}>
-      <Stack.Screen 
-        name="Signup" 
-        component={Signup} 
-        options={{ title: 'Signup' }}
-      />       
-      <Stack.Screen 
-        name="Login" 
-        component={Login} 
-        options={
-          {title: 'Login'},
-          {headerLeft: null} 
-        }
-      />
-      <Stack.Screen 
-       name="Verification" 
-       component={Verification} 
-       options={
-         { title: 'Verification' },
-         {headerLeft: null} 
-       }
-      />
-      
-    </Stack.Navigator>
-  );
-}
+       },
+       iconHeader: {
+           color: "black",
+       },
+       
+       inputStyle: {
+        width: '100%',
+        marginBottom: 15,
+        paddingBottom: 15,
+        alignSelf: "center",
+        borderColor: "#ccc",
+        borderBottomWidth: 1
+      },
 
-export default function App() {
-  return (
-    <NavigationContainer>
-      <MyStack />
-    </NavigationContainer>
-  );
-}
+      subTitle: {
+        color: '#000000',
+        textAlign: 'center',
+        paddingVertical: 20,
+        fontSize: 20,
+        fontWeight: '600',
+
+    },
+       
+       buttonVerifyWrrapper: {
+        alignItems: 'center',
+        marginVertical: 10,
+
+    },
+    buttonVverify: {
+       backgroundColor: '#000000',
+       paddingHorizontal: 30,
+       paddingVertical: 20,
+       width: '100%',
+       alignItems: 'center',
+       borderRadius: 10,
+    },
+    textButtonVeerify: {
+        color: '#fff',
+        fontSize: 18,
+        fontWeight: 'bold',
+        textTransform: 'uppercase',
+    },
+    
+       
+    });
+
+    return (
+        <View style={styles.container}>
+       
+        <SafeAreaView>
+        <View style={StyleSheet.header}>
+        <TouchableOpacity> 
+        <Icon name="chevron-left" size={24} style={styles.iconHeader}/>
+
+        </TouchableOpacity>
+        <View>
+        <Text style={styles.headerTitle}>Welcome To Mopedo</Text>
+        </View>
+        <View
+        style={{
+            width: 40,
+            
+        }}
+        />
+        </View>
+        </SafeAreaView>
+        
+        <TextInput
+          style={styles.inputStyle}
+          placeholder="Mobile. No"
+         
+          onChangeText={(val) => this.updateInputVal(val, 'phonenumber')}
+          
+        />
+
+<View style={styles.content}>
+            <Text style={styles.title}>Note You will recieve 4-digit OTP code on entered mobile number for verification</Text>
+            
+            </View>
+        
+       
+                  
+                  <View style={styles.buttonVerifyWrrapper}>
+                      <TouchableOpacity style={styles.buttonVverify}>
+                          <Text style={styles.textButtonVeerify}>Get OTP</Text>
+                      </TouchableOpacity>
+                  </View>
+                  
+                    
+              
+                                 
+                                 </View>
+                                 
+                                 
+                                 
+    );
+                                }
